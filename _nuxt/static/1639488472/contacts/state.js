24ec80f1f7a98d33ec5358c8fdc5c2a85111186c
likhachev-ara <mac452ares@gmail.com>
2021-12-14 18:27:54 +0500

    Updates

---
+++ _nuxt/static/1639488472/contacts/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Flanding\u002F_nuxt\u002Fstatic\u002F1639488472",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcontacts",config:{_app:{basePath:"\u002Flanding\u002F",assetsPath:"\u002Flanding\u002F_nuxt\u002F",cdnURL:a}}}}(null));
