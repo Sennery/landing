8bcd23348261080608617ded5d31e7d3fd42a232
likhachev-ara <mac452ares@gmail.com>
2022-02-17 21:33:11 +0500

    Updates

--- _nuxt/static/1645100877/about/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Flanding\u002F_nuxt\u002Fstatic\u002F1645100877",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002Flanding\u002F",assetsPath:"\u002Flanding\u002F_nuxt\u002F",cdnURL:a}}}}(null));
